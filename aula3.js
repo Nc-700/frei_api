3bc98f82b7e116d964adb68bb8fe32dc6c3dc271
Nicolas <Nicolasbatista789@yahoo.com>
2024-08-02 11:26:03 -0300

    ok

---
+++ aula3.js
@@ -0,0 +1,23 @@
+import express from "express";
+
+const servidor = express();
+
+servidor.get('/calculadora/somar/:n1/:n2',(req,resp)=> {
+
+    let n1 =Number (req.params.n1);
+    let n2 = Number (req.params.n2);
+
+let soma = n1 + n2;
+
+resp.send('a soma é '+ soma);
+})
+
+
+
+
+
+
+
+
+
+servidor.listen(5050, () => console.log('api subiu na porta 5050'));
